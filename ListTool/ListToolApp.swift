38680fbb589d4e36f09646bd2ce209d75a39c379
Anthony <anthonyoutub@gmail.com>
2023-01-01 16:23:26 +0100

    init

---
+++ ListTool/ListToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListToolApp.swift
+//  ListTool
+//
+//  Created by Anthony RODRIGUES on 01/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct ListToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
